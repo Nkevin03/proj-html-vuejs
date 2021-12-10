48fc3a5addfcc24fa23d4d76bb5d8ec32ec73d68
Nkevin03 <knezosi6@gmail.com>
2021-12-10 14:06:20 +0100

    Commit hero

--- instructor/src/components/Header.vue
+++ instructor/src/components/Header.vue
@@ -1,17 +1,159 @@
 <template>
-    <Navbar />
+<section>
+        <header>
+        <head>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer">
+            <link rel="icon" href="<%= BASE_URL %>favicon.ico">
+        </head>
+        <div class="logo-navbar">
+            <a href="#"><img src="@/assets/img/MasterStudy-1.svg" alt=""></a>
+        </div>
+        <div class="right-navbar">
+            <ul>
+                <li v-for="link in NavbarLink" :key="`${link.text}`">
+					<a :href="`${link.url}`">
+						{{link.name}}
+					</a>
+                     <div class="arrow"></div>
+				</li>
+            </ul>
+            <div class="social-header">
+                <i class="fab fa-linkedin"></i>
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-facebook"></i>
+                <i class="fab fa-twitter"></i>
+            </div>
+        </div>
+    </header>
+    <Hero />
+</section>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
 export default {
     name: 'Header',
     components: {
-        Navbar
-    }
+    Hero
+    },
+    data() {
+		return {
+			NavbarLink: [
+				{
+					name: 'Home',
+                    span: false,
+					url: '#',
+				},
+				{
+					name: 'Course Formats',
+                    span: false,
+					url: '#',
+				},
+				{
+					name: 'Add Course',
+                    span: true,
+					url: '#',
+				},
+				{
+					name: 'Pages',
+                    span: false,
+					url: '#',
+				},
+				{
+					name: 'Demons',
+                    span: false,
+					url: '#',
+				}
+			],
+		};
+	},
+
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+@import '@/styles/preset.scss';
+    header{
+        width: 70%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
 
+        .logo-navbar{
+            display: flex;
+            align-items: center;
+            
+            img{
+                cursor: pointer;
+            }
+        }
+        .right-navbar{
+            display: flex;
+            align-items: center;
+            line-height: 100px;
+
+            ul{
+                display: flex;
+                list-style: none;
+
+                li{
+                    line-height: 165px;
+                    position: relative;
+
+                    &:hover{
+                        border-bottom: 3px solid $kourmikova;
+                        display: flex;
+                        justify-content: center;
+
+                        .arrow{
+                            position: absolute;
+                            bottom: 0;
+                            width: 0; 
+                            height: 0; 
+                            border-left: 5px solid transparent;
+                            border-right: 5px solid transparent;
+                            border-bottom: 5px solid $kourmikova;
+                        }
+                    }
+
+                    span{
+                        position: absolute;
+                        top: 45px;
+                        right: 0;
+                        background: $red;
+                        line-height: 22px;
+                        font-weight: 500;
+                        width: 48px;
+                        text-align: center;
+                        border-top-left-radius: 10px;
+                        border-top-right-radius: 10px;
+                        border-bottom-right-radius: 10px;
+                        border-bottom-left-radius: 3px;
+                        color: $white;
+                        font-size: 13px;
+                    }
+                }
+
+                a{
+                    text-decoration: none;
+                    color: $shaft;
+                    font-size: 20px;
+                    padding: 0 10px;
+                    font-weight: 700;
+                }
+            }
+            .social-header{
+                padding-left: 40px;
+                i{
+                    cursor: pointer;
+                    padding-right: 10px;
+                    font-size: 20px;
+                    color: $shaft;
+                }
+            }
+        }
+    }
+    
+        
 </style>
---
+++ instructor/src/components/Hero.vue
@@ -0,0 +1,170 @@
+<template>
+    <section class="hero">
+        <div class="hero-content">
+           <div class="hero-text">
+                <h1>Start your human <span>Revolution</span> today</h1>
+                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+           </div>
+        </div>
+        <div class="sales">
+            <div class="box-sales">
+                <div class="box-left">
+                    <h3><span>Limited Sale: </span>All courses with 55% off</h3>
+                    <div>
+                        <input type="text" placeholder="Enter your E-mail">
+                        <a href="#">SUBSCRIBE</a>
+                    </div>
+                </div>
+                <div class="box-right">
+                    <div class="item">
+                        <h3>26</h3>
+                        <p>Days</p>
+                    </div>
+                    <div class="item">
+                        <h3>01</h3>
+                        <p>Hours</p>
+                    </div>
+                    <div class="item">
+                        <h3>47</h3>
+                        <p>Minutes</p>
+                    </div>
+                    <div class="item">
+                        <h3>34</h3>
+                        <p>Seconds</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Hero',
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/preset.scss';
+    .hero{
+        background-image: url(../assets/img/slide-1.jpg);
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 90vh;
+
+        .hero-content{
+            display: flex;
+            justify-content: end;
+            align-items: center;
+            height: 100%;
+            width: 60%;
+            margin: 0 auto;
+            
+            .hero-text{
+                display: flex;
+                flex-direction: column;
+                width: 60%;
+                transform: translateY(-100px);
+
+                h1{
+                    width: 70%;
+                    font-size: 60px;
+
+                    span{
+                        color: $fuel-yellow;
+                    }
+                }
+
+                p{
+                    margin-top: 30px;
+                    font-weight: 300;
+                    line-height: 30px;
+                    font-size: 20px;
+                }
+            }
+        }
+        .sales{
+            display: flex;
+            justify-content: center;
+
+            .box-sales{
+                display: flex;
+                height: 250px;
+                width: 1300px;
+                transform: translateY(-120px);
+                border-radius: 15px;
+                background-color: $buttercup;
+
+                .box-left{
+                    width: 50%;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    flex-direction: column;
+
+                    div{
+                        display: flex;
+                        align-items: center;
+                        position: relative;
+                        width: 85%;
+                        padding: 30px 0;
+
+                        input{
+                            width: 100%;
+                            height: 50px;
+                            border: none;
+                            outline: none;
+                            padding: 0 30px;
+                            border-radius: 25px;
+                        }
+
+                        a{
+                            position: absolute;
+                            background-color: $firefly;
+                            color: #fff;
+                            text-decoration: none;
+                            height: 50px;
+                            text-align: center;
+                            display: flex;
+                            align-items: center;
+                            border-radius: 25px;
+                            padding: 0 20px;
+                            right: 0;
+                        }
+                    }
+
+                    h3{
+                        font-size: 30px;
+                        color: $shaft;
+
+                        span{
+                            color: #fff;
+                        }
+                    }
+                }
+                .box-right{
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    width: 50%;
+
+                    .item{
+                        padding: 30px;
+
+                        h3{
+                            color: #fff;
+                            font-size: 70px;
+                        }
+                        p{
+                            font-size: 20px;
+                            color: $black;
+                            text-align: center;
+                            font-weight: 700;
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
